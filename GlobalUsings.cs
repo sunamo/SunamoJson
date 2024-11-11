51e988df2377c3bf8e5f8dfe2ef83c903dfea836
sunamo.cz <radek.jancik@sunamo.cz>
2024-11-11 16:12:48 +0100

    24.11.11.1 Fix empty string for spaces

--- GlobalUsings.cs
+++ GlobalUsings.cs
@@ -8,3 +8,5 @@ global using System.Diagnostics;
 global using System.Reflection;
 global using System.Net;
 global using System.Diagnostics.CodeAnalysis;
+global using SunamoJson;
+global using SunamoJson._sunamo;

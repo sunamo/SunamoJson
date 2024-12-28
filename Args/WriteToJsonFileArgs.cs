5088f1e1dadb7872d13a3a8fda238cc42c1e9839
sunamo.cz <radek.jancik@sunamo.cz>
2024-12-28 11:52:11 +0100

    Updated comment

--- Args/WriteToJsonFileArgs.cs
+++ Args/WriteToJsonFileArgs.cs
@@ -1,9 +1,13 @@
-namespace SunamoJson.Args;
+﻿namespace SunamoJson.Args;
 
 
 public class WriteToJsonFileArgs
 {
     public Newtonsoft.Json.Formatting Formatting { get; set; } = Newtonsoft.Json.Formatting.None;
     public bool Append { get; set; } = false;
+    /// <summary>
+    /// Nebude fungovat pro deserializaci ze souboru
+    /// Při deserializaci by se mi udělalo \\" z \" - musel bych mít seznam všeho co navrátit
+    /// </summary>
     public bool TwoBackslashToSingle { get; set; } = false;
 }

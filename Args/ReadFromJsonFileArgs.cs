420969207f0ea79f0b40c8807e040dff4b486611
sunamo.cz <radek.jancik@sunamo.cz>
2025-03-05 07:29:17 +0100

    Added global using of Microsoft.Extensions.Logging.Abstractions to all projects

--- Args/ReadFromJsonFileArgs.cs
+++ Args/ReadFromJsonFileArgs.cs
@@ -1,11 +1,5 @@
 namespace SunamoJson.Args;
 
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
 public class ReadFromJsonFileArgs
 {
     /// <summary>

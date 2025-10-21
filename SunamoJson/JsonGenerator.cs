69f5083ce5778befcb1ab4f43168395530581867
Radek Jancik <radek.jancik@usys.cz>
2025-10-21 20:42:47 +0200

    feat: Replace variables names with self-describe names

--- SunamoJson/JsonGenerator.cs
+++ SunamoJson/JsonGenerator.cs
@@ -1,16 +1,19 @@
+// EN: Variable names have been checked and replaced with self-descriptive names
+// CZ: Názvy proměnných byly zkontrolovány a nahrazeny samopopisnými názvy
+
 namespace SunamoJson;
 
 public class JsonGenerator
 {
-    private readonly StringBuilder sb = new();
+    private readonly StringBuilder stringBuilder = new();
 
     public void Pair(string key, string value)
     {
-        sb.AppendLine("\"" + key + "\": " + "\"" + value + "\",");
+        stringBuilder.AppendLine("\"" + key + "\": " + "\"" + value + "\",");
     }
 
     public override string ToString()
     {
-        return sb.ToString();
+        return stringBuilder.ToString();
     }
 }
